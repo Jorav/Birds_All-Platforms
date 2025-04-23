95f46997ed20fe68d348abc845f1092e338bce64
Jorav <johan@ravnborg.se>
2025-04-23 21:05:38 +0200

    Added contains in AABB and interface

--- Birds/src/bounding_areas/IBoundingArea.cs
+++ Birds/src/bounding_areas/IBoundingArea.cs
@@ -2,19 +2,21 @@ using Microsoft.Xna.Framework;
 
 namespace Birds.src.bounding_areas
 {
-    public interface IBoundingArea
-    {
-        public Vector2 Position { get; set; }
-        public float Radius { get; }
-        //public float Scale {get; set;}
+  public interface IBoundingArea
+  {
+    public Vector2 Position { get; set; }
+    public float Radius { get; }
+    //public float Scale {get; set;}
 
-        bool CollidesWith(IBoundingArea boundingArea);
+    bool CollidesWith(IBoundingArea boundingArea);
 
-        //returns a tuple with the maximum X positions and maximum y position of the whole object (these two values does not necessarily belong to the same point)
-        public (float, float) MaxXY {get;}
+    //returns a tuple with the maximum X positions and maximum y position of the whole object (these two values does not necessarily belong to the same point)
+    public (float, float) MaxXY { get; }
 
-        //returns a tuple with the minimum X positions and minimum y position of the whole object (these two values does not necessarily belong to the same point)
-        public (float, float) MinXY{get;}
+    //returns a tuple with the minimum X positions and minimum y position of the whole object (these two values does not necessarily belong to the same point)
+    public (float, float) MinXY { get; }
+
+    public bool Contains(Vector2 position);
 
     }
 }
